f78d5ca83db80395469a422822c93ddd73344d7d
cag2050 <cag2050@163.com>
2017-12-22 18:42:42 +0800

    m

--- array_sort_by_object_key.js
+++ array_sort_by_object_key.js
@@ -1,15 +1,21 @@
-let arr = [{dt: '20171119', index: '1'}, {dt: '20171118', index: '2'}, {dt: '20171120', index: '3'}];
+let arr = [{dt: '20171119', index: '1'}, {dt: '20171118', index: '2'}, {dt: '20171120', index: '3'}]
 
 let compare = function (obj1, obj2) {
-    let value1 = obj1.dt
-    let value2 = obj2.dt
+    let value1, value2
+    if (obj1 && obj1.dt) {
+        value1 = obj1.dt
+    }
+    if (obj2 && obj2.dt) {
+        value2 = obj2.dt
+    }
     if (value1 < value2) {
-        return 1;
+        return 1
     } else if (value1 > value2) {
-        return -1;
+        return -1
     } else {
-        return 0;
+        return 0
     }
 }
-arr.sort(compare);
+
+arr.sort(compare)
 console.log(arr)
