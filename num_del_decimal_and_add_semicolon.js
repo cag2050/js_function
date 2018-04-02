8624923548c423421dfe36ea9c7b0b2265085793
cag2050 <cag2050@163.com>
2018-04-02 15:32:34 +0800

    m

---
+++ num_del_decimal_and_add_semicolon.js
@@ -0,0 +1,6 @@
+// 数字去掉小数，再加上分号
+let num = 1234567891.44
+// console.log(typeof parseInt(num.toString()))
+// console.log(parseInt(num.toString()))
+console.log(num.toLocaleString())
+// parseInt(num.toString()).toLocaleString()
