3331bdae5318f28803c29c2e07d762fbe799530e
cag2050 <cag2050@163.com>
2018-04-12 17:33:39 +0800

    m

--- array_map.js
+++ array_map.js
@@ -1,19 +1,16 @@
 // map() 方法返回一个新数组，数组中的元素为原始数组元素调用函数处理后的值。
 // map() 方法按照原始数组元素顺序依次处理元素。
 // 注意： map() 不会对空数组进行检测。
-// 注意： map() 不会改变原始数组。
+// 注意： map() 不会改变原始数组。如果对原数组的值进行操作，会改变原数组。
 
 let arr = [
     {key: 'name', value: 'cag'},
     {key: 'age', value: '18'}
 ]
 
-// let newArr = arr.map((item, index, thisArr) => {
-//     return item.key = item.key + '111'
-// })
-
-let newArr = arr.map(function (item, index, thisArr) {
+let newArr = arr.map((item, index, thisArr) => {
     return item.key = item.key + '111'
 })
+
 console.log(arr)
 console.log(newArr)
