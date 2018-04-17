1dcc75e9cd470705a596e65f8cb207c7a4b82f52
cag2050 <cag2050@163.com>
2018-04-17 14:56:45 +0800

    m

--- string_localeCompare.js
+++ string_localeCompare.js
@@ -2,10 +2,7 @@
 let arr = ['汉字', '你', '啊']
 
 arr.sort((a, b) => {
-    // return a.toLocaleString().localeCompare(b.toLocaleString(), 'zh')
-    // return a.toString().localeCompare(b.toString())
-    // return a.localeCompare(b, 'zh')
-    return a.localeCompare(b)
+    return a.toString().localeCompare(b.toString())
 })
 
 console.log(arr)
