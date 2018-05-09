550a073fc7397950f85fd523018e0f0d27efb7cd
cag2050 <cag2050@163.com>
2018-05-09 14:44:08 +0800

    m

--- array_max.js
+++ array_max.js
@@ -1,3 +1,15 @@
-let arr = [1, 2, 3]
-let max = Math.max(...arr)
+// 求对象数组中，某个对象属性的最大值
+
+let arr = [
+    {value: 1},
+    {value: 2},
+    {value: 211}
+]
+let newArr = arr.map(
+    (item) => {
+        return item.value
+    }
+)
+console.log(newArr)
+let max = Math.max(...newArr)
 console.log(max)
