a3397208bd2042f882fbc6f2c2dfa1911e12a64b
cag2050 <cag2050@163.com>
2018-05-15 20:14:08 +0800

    m

---
+++ number.toFixed(0).toLocaleString().js
@@ -0,0 +1,7 @@
+// toFixed：将数字转成了字符串
+let number = 10000.10
+console.log(number.toFixed(0))
+console.log(typeof number.toFixed(0))
+console.log(number.toLocaleString())
+console.log(parseInt(number.toFixed(0)).toLocaleString())
+console.log(typeof number.toFixed(0).toLocaleString())
