e5fbcbf1dce911465aa167ba63cc9730da90c5fa
cag2050 <cag2050@163.com>
2018-08-14 20:10:20 +0800

    m

---
+++ 正则已#开头.js
@@ -0,0 +1,3 @@
+let str = '#sfsfsf'
+
+console.log(!/^#/.test(str))
--- 正则提取url参数-参考.js
+++ 正则提取url参数-参考.js
@@ -1,13 +1,32 @@
-let urlParam = '#埋点日志\neventid=事件id\nparam1=111\nparam2=222'
-// var urlParam = '?foo=123&bar=&baz=789';
+let urlParam = '#埋点日志\neventId=事件id\nparam1=111\nparam2=222'
+// let urlParam = '?foo=123&bar=&baz=789';
 
-// console.log(str.match(/#(\S*)\n/)[1])
-// console.log(str.match(/eventid=(\S*)\n/)[1])
-// var regExp = /(([^?&=]+)(?:=([^?&=]*))*)/g;
-   var regExp = /(([^(\n)=]+)(?:=([^(\n)=]*))*)/g;
-var match;
-while (match = regExp.exec(urlParam)) {
+// console.log(urlParam.match(/#(\S*)\n/)[1])
+// console.log(urlParam.match(/eventid=(\S*)\n/i)[1])
+// let regExp = /(([^?&=]+)(?:=([^?&=]*))*)/g;
+   let regExp = /(([^(\n)=]+)(?:=([^(\n)=]*))*)/g;
+// let match;
+let match = regExp.exec(urlParam)
+let resultArr = []
+/*
+while (regExp.exec(urlParam)) {
+    match = regExp.exec(urlParam)
+    console.log(match)
+}
+*/
+while (match) {
     // console.log(match)
     console.log(`${match[2]} = ${match[3]}`)
+    if (/^#/.test(match[2]) || match[2] === 'eventid') {
 
+    } else {
+        // result[match[2]] = match[3]
+        resultArr.push({
+            inputValue1: match[2],
+            inputValue2: match[3]
+        })
+
+    }
+    match = regExp.exec(urlParam)
 }
+console.log(resultArr)
